34981c163e5b6dbaca05effb15775acfb928d287
richardli03 <richardli485@gmail.com>
2025-02-13 21:49:52 -0500

    BLINKY SOFTWARE

---
+++ software/blinky/_ide/workspace_journal.py
@@ -0,0 +1,56 @@
+# 2025-02-13T20:51:15.146516
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="blinky")
+
+platform = client.create_platform_component(name = "platform",hw_design = "$COMPONENT_LOCATION/../blinky_wrapper.xsa",os = "standalone",cpu = "ps7_cortexa9_0",domain_name = "standalone_ps7_cortexa9_0")
+
+platform = client.get_component(name="platform")
+status = platform.build()
+
+comp = client.get_component(name="xgpio_example")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+client.delete_component(name="xgpio_low_level_example")
+
+client.delete_component(name="xgpio_tapp_example")
+
+status = platform.build()
+
+comp = client.get_component(name="xgpio_low_level_example")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp = client.get_component(name="xgpio_example")
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
+status = platform.build()
+
+comp.build()
+
