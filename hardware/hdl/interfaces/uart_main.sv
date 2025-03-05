23870b20cbac16dc6cf61a9d5f3219a973a3003a
Drew <silverbird337@gmail.com>
2025-03-05 02:00:25 -0500

    add next state sequencer

--- hardware/hdl/interfaces/uart_main.sv
+++ hardware/hdl/interfaces/uart_main.sv
@@ -2,34 +2,79 @@
 Generic uart main
 */
 
-`begin_keywords "1800-2017" // Use SystemVerilog 2023 keywords
+`begin_keywords "1800-2017"  // Use SystemVerilog 2017 keywords
 `default_nettype none
 
 module uart_main (
-    input rstN,
-    input clk,
+    input logic rst_n,
+    input logic clk,
 
-    // uart
-    input rx,
-    output tx,
-    input mode, // determines read/write
-    output read_data,
-    input write_data,
+    // uart // TODO: split based on rx tx
+    input  logic rx,
+    output logic tx,
+    input  logic mode,       // determines read/write
+    output logic read_data,
+    input  logic write_data,
 
     // ready-valid
-    input read_ready,
-    input write_valid,
-    output read_valid,
-    output write_ready
+    input  logic read_ready,
+    input  logic write_valid,
+    output logic read_valid,
+    output logic write_ready
 );
-    timeunit 1ns; timeprecision 100ps;
+  timeunit 1ns; timeprecision 100ps;
 
-    // parameters
-    parameter buffer_width = 8; // the input and ouput fifo widths
+  // parameters
+  parameter BUFFER_WIDTH = 8;  // input and ouput fifo widths
+  parameter BAUD_RATE = 115200;
 
-    // local parameters
+  // local parameters
+  localparam BUFFER_COUNTER_BITS = $clog2(BUFFER_WIDTH);
 
-endmodule: uart_main
+  // uart read states
+  typedef enum logic [3:0] {
+    RESET = 4'b0000,
+    IDLE  = 4'b0001,
+    START = 4'b0010,
+    READ  = 4'b0100,
+    ERROR = 4'b1000
+  } uart_rx_state_t;
 
-`default_nettype wire
-`end_keywords
+  // internal state variables
+  uart_rx_state_t rx_state, rx_next_state;
+  logic [BUFFER_COUNTER_BITS-1:0] rx_counter;
+
+  // rx current state logic
+  always_ff @(posedge clk or negedge rst_n)
+    if (!rst_n) rx_state <= RESET;
+    else rx_state <= rx_next_state;
+
+  // next state logic
+  always_ff @(posedge clk)
+    unique case (rx_state)
+      RESET: rx_next_state <= RESET;
+      IDLE:
+        if (!rx)
+          rx_next_state <= START;
+        else
+          rx_next_state <= IDLE;
+      START:
+        rx_next_state <= READ;
+      READ:
+        if (rx_counter  == '0)
+          rx_next_state <= IDLE;
+        else
+          rx_next_state <= READ;
+      ERROR:
+        rx_next_state <= ERROR;
+      default:
+        rx_next_state <= ERROR; // catch glitches
+
+    endcase
+
+  // fsm outputs
+
+
+endmodule : uart_main
+
+`default_nettype wire `end_keywords
