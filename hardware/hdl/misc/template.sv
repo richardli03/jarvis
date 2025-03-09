1fd3b231f68a69783c837736bee32f6c0e5c2ece
Drew <silverbird337@gmail.com>
2025-03-09 13:19:25 -0400

    add module template

---
+++ hardware/hdl/misc/template.sv
@@ -0,0 +1,31 @@
+/*
+description
+
+@param A_PARAMETER
+*/
+
+`begin_keywords "1800-2017"  // Use SystemVerilog 2017 keywords
+`default_nettype none
+
+module template (
+    input logic rst_n,
+    input logic clk
+);
+  timeunit 1ns; timeprecision 100ps;
+
+  // parameters
+  parameter A_PARAMETER = 8;
+
+  // local parameters
+  localparam ANOTHER_PARAMETER = $clog2(A_PARAMETER);
+
+/* 
+ * =============================================================================
+ * section header
+ * =============================================================================
+ */
+
+endmodule : template
+
+`default_nettype wire 
+`end_keywords
